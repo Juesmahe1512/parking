ab54c6c257fa2cee3004a2c7a0366c435903ac75
Juan Esteban Mazo Herrera - Ceiba Software <juan.mazo@MacBook-Pro-de-soporteceibalocal.local>
2020-12-29 11:05:56 -0500

    se crea dataclass Parqueadero

---
+++ app/src/main/java/com/example/parking/entidades/Parqueadero.kt
@@ -0,0 +1,10 @@
+package com.example.parking.entidades
+
+data class Parqueadero(
+    var placa: String,
+    var cilindrada: String,
+    var tipoVehiculo: String,
+    var capacidadCarros: String,
+    var capacidadMotos: String
+
+    )
